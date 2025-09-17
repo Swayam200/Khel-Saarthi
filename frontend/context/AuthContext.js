6902a76085841e130ed258e5c4aefabfc3e2807d
Swayam <swayam.panda200@gmail.com>
2025-09-17 14:01:30 +0530

    Fixed Socket Import Issue

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { io } from 'socket-io-client';
+import { io } from 'socket.io-client';
 import Constants from 'expo-constants';
 import Toast from 'react-native-toast-message';
 import api from '../api/api';
